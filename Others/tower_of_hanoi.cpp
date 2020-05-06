3f143418f36012fc827fcb2f9e729577095159a1
Pratik Jain <57676701+jainpratik830@users.noreply.github.com>
2020-05-06 11:13:46 +0530

    Add files via upload

---
+++ Others/tower_of_hanoi.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int count=0;
+void toh(int n,char src,char aux,char dest)
+{
+
+	if (n>0)
+	{	toh(n-1,src,dest,aux);
+		count++;
+		cout<<count << " Moving disc "<< src<< " to " << dest <<endl;
+		toh(n-1,aux,src,dest);
+
+	}
+};
+int main()
+{
+ int a;
+ cout <<"Enter number of discs: ";
+ cin>> a;
+ cout << endl;
+ toh(a,'A','B','C');
+ return 0;
+}
+
